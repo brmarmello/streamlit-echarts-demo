38a9467bf006413d71f422258f106b1420d97881
Bruno Marmello <brmarmello@gmail.com>
2023-11-02 08:15:58 +0000

    Chore: Change Sidebar.

--- app.py
+++ app.py
@@ -62,10 +62,10 @@ if __name__ == "__main__":
     with st.sidebar:
         st.markdown("---")
         st.markdown(
-            '<h6>Made in &nbsp<img src="https://streamlit.io/images/brand/streamlit-mark-color.png" alt="Streamlit logo" height="16">&nbsp by <a href="https://twitter.com/andfanilo">@andfanilo</a></h6>',
+            '<h6>Orgulhosamente Produzido no Streamlit &nbsp<img src="https://streamlit.io/images/brand/streamlit-mark-color.png" alt="Streamlit logo" height="16">&nbsp Por <a href="https://www.linkedin.com/in/brunomarmello">MarTechBM</a></h6>',
             unsafe_allow_html=True,
         )
         st.markdown(
-            '<div style="margin-top: 0.75em;"><a href="https://www.buymeacoffee.com/andfanilo" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a></div>',
+            '<div style="margin-top: 0.75em;"><a href="https://www.buymeacoffee.com/brmarmello" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a></div>',
             unsafe_allow_html=True,
         )
